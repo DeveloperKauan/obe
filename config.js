d91d73b9f2e8a3f25cf16cd69ae35ac00782c716
Kauan <166316814+DeveloperKauan@users.noreply.github.com>
2025-04-04 20:11:09 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,17 @@
+// backend/db/config.js
+
+const sql = require('mssql');
+
+const config = {
+  user: 'SEU_USUARIO_SQL',
+  password: 'SUA_SENHA_SQL',
+  server: 'localhost', // ou o IP do servidor
+  database: 'OBE',
+  options: {
+    encrypt: false, // true se estiver usando Azure
+    trustServerCertificate: true
+  }
+};
+
+module.exports = config;
 
